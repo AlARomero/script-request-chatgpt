01accc14f8d79cde96594571fc904e941aa9074d
AIARomero <alberto.almagro.06@gmail.com>
2023-03-19 20:40:11 +0100

    No entiendo pero hay un bug cuando solo se envia 1 palabra y lo he arreglado, minimo 2 palabras

--- gpt.py
+++ gpt.py
@@ -6,15 +6,22 @@ import argparse
 load_dotenv()
 
 token = os.environ.get('GPT_TOKEN')
-do_completion = True
+do_completion = False
 
 parser = argparse.ArgumentParser()
 parser.add_argument("pregunta")
 args = parser.parse_args()
+
+index = 0
+while not do_completion and index < len(args.pregunta):
+    if args.pregunta[index] == " ":
+        do_completion = True
+    index+=1 
 
 if do_completion:
     openai.api_key = token
     completion = openai.Completion.create(engine = "text-davinci-003", prompt = args.pregunta, max_tokens = 3000)
     response = completion.choices[0].text[1:]
     print(response + "\n")
-
+else:
+    print("\nLongitud minima de la pregunta de 2 palabras\n")
